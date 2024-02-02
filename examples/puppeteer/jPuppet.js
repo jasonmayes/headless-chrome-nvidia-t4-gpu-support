868a595adf1d5f99bbe50fe440b429562ca0607c
Jason Mayes <4972997+jasonmayes@users.noreply.github.com>
2024-02-02 10:32:52 -0800

    Enable capture of chrome:// urls

--- examples/puppeteer/jPuppet.js
+++ examples/puppeteer/jPuppet.js
@@ -24,7 +24,8 @@ async function runWebpage() {
         '--no-first-run',
         '--disable-features=Translate',
         '--no-default-browser-check',
-        '--window-size=1280,720'
+        '--window-size=1280,720',
+        '--allow-chrome-scheme-url'
       ]
   });
 
